fe15bae767d54d32c1eb8e71b1dcfb8fee71ebcd
marcovidali2 <marcovidali6@proton.me>
2024-05-14 15:49:32 +0200

    feat(app): Set up project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sorting Algorithms Visualizer
+
+A simple sorting algorithms visualizer website made with ReactJS
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useSetTheme } from "./ui/useSetTheme";
+
+const App = () => {
+    useSetTheme();
+
+    return <div>Sorting Algorithms Visualizer</div>;
+};
+
+export default App;
