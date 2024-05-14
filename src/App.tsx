0897ec7d7129dcee9082da93741679bfa45c2846
marcovidali2 <marcovidali6@proton.me>
2024-05-14 16:02:37 +0200

    feat(app): Implement form ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 import { useSetTheme } from "./lib/useSetTheme";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "./components/ui/select";
+
+const sortingAlgorithmOptions = ["Bubble sort", "Quick sort"];
 
 const App = () => {
     useSetTheme();
 
-    return <div>Sorting Algorithms Visualizer</div>;
+    return (
+        <div className="flex flex-col items-center p-4">
+            <form className="flex gap-4">
+                <Input
+                    placeholder='Numbers array (separated by a ",")'
+                    className="w-96"
+                />
+
+                <Select>
+                    <SelectTrigger className="w-48">
+                        <SelectValue>Sorting algorithm</SelectValue>
+                    </SelectTrigger>
+
+                    <SelectContent>
+                        <SelectGroup>
+                            {sortingAlgorithmOptions.map(
+                                (sortingAlgorithmOption) => (
+                                    <SelectItem value={sortingAlgorithmOption}>
+                                        {sortingAlgorithmOption}
+                                    </SelectItem>
+                                ),
+                            )}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
+                <Button>Sort!</Button>
+            </form>
+        </div>
+    );
 };
 
 export default App;
